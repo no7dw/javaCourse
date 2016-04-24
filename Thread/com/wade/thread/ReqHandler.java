a00cd5738415b64ecadfafafea7350cb30675097
no7david <wdeng@kalengo.com>
2016-04-24 23:54:54 +0800

    fix error status code

--- Thread/com/wade/thread/ReqHandler.java
+++ Thread/com/wade/thread/ReqHandler.java
@@ -7,7 +7,7 @@ import java.nio.charset.StandardCharsets;
 public class ReqHandler {
     //throws IOException
     public static void handleRequest(Socket socket) throws IOException {
-        final String response = "HTTP/1.0 200OK\r\n" + "Content-type:text/plain\r\n" + "\r\n" + "Hello World\r\n";
+        final String response = "HTTP/1.0 200 OK\r\n" + "Content-type:text/plain\r\n" + "\r\n" + "Hello World\r\n";
         // Read the input stream, and return "200 OK"
         try{
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
