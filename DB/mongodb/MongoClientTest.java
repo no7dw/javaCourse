ef6a3a895a596e23e9d58aebf22adf93aac9e5ca
no7david <wdeng@kalengo.com>
2016-05-01 22:40:10 +0800

    with update ,count

--- DB/mongodb/MongoClientTest.java
+++ DB/mongodb/MongoClientTest.java
@@ -3,7 +3,10 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
+
+import java.lang.System;
 
 public class MongoClientTest 
 {
@@ -16,11 +19,28 @@ public class MongoClientTest
 		MongoCollection<Document> collection = db.getCollection("user_account_cashflow");
 		System.out.println("集合 user_account_cashflow 选择成功");
 
-		FindIterable<Document> findIterable = collection.find(new Document("type", 2));  
+		//count with two condiction
+		Long number = collection.count(new Document("type",2).append("user_id", "54ce55b06f48531b4bd275fc"));
+		System.out.printf("query has docs count %d\n", number);
+		//query inter with two condiction
+		FindIterable<Document> findIterable = 
+		collection
+		.find(new Document("type", 2).append("user_id", "54ce55b06f48531b4bd275fc"))
+		.sort(new Document("user_id", 1))
+		.limit(3);
+
+		//cursor output each
          MongoCursor<Document> mongoCursor = findIterable.iterator();  
          while(mongoCursor.hasNext()){  
             System.out.println(mongoCursor.next());  
          }  
+         collection.updateMany(Filters.eq("user_id", "54ce55b06f48531b4bd275fc"),
+         	new Document("$set",new Document("user_id","54cef05579337f164b365051")));
+
+		//re count to see the change
+		number = collection.count(new Document("type",2).append("user_id", "54ce55b06f48531b4bd275fc"));
+		System.out.printf("query has docs count %d\n", number);
+
 
 	}
 }
