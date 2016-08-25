c2d9bce530d162b7ab6f71cfb4b37bf51a6956b2
no7dw <no7david@gmail.com>
2016-08-26 01:35:18 +0800

    add mongo

--- SpringMVCRestAPI/src/main/java/com/wade/controller/SpringMVCRESTController.java
+++ SpringMVCRestAPI/src/main/java/com/wade/controller/SpringMVCRESTController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,55 +15,55 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SpringMVCRESTController {
-    
+    @Autowired
+    private CustomerRepository repository;
+
     @RequestMapping(value = "/populateDataFromServer/{name}",  method=RequestMethod.GET, produces={"application/json"})
-    public @ResponseBody List<Integer> populateActivePSwapBasketGET(@PathVariable String name) {                
-        
-        if (name.equalsIgnoreCase("JavaHonk")) {
-            return returnDataList();
-        }else {
-            List<Integer> list = new ArrayList<Integer>();
-            list.add(12345);
-            return list;
+    public @ResponseBody List<Customer> populateActivePSwapBasketGET(@PathVariable String name) { 
+        List<Customer> list = new ArrayList<Customer>();                
+        for (Customer customer : repository.findByLastName("Smith")) {
+            System.out.println(customer);
+            list.add(customer);
         }
+        return list;
         
     }
 
-    @RequestMapping(value = "/populateDataFromServer", method=RequestMethod.POST, produces={"application/json"})
-    public @ResponseBody List<Integer> populateActivePSwapBasketPOST(@RequestParam String name) {               
+    // @RequestMapping(value = "/populateDataFromServer", method=RequestMethod.POST, produces={"application/json"})
+    // public @ResponseBody List<Integer> populateActivePSwapBasketPOST(@RequestParam String name) {               
         
-        if (name.equalsIgnoreCase("JavaHonk")) {
-            return returnDataList();
-        }else {
-            List<Integer> list = new ArrayList<Integer>();
-            list.add(12345);
-            return list;
-        }
-    }
+    //     if (name.equalsIgnoreCase("JavaHonk")) {
+    //         return returnDataList();
+    //     }else {
+    //         List<Integer> list = new ArrayList<Integer>();
+    //         list.add(12345);
+    //         return list;
+    //     }
+    // }
     
-    @RequestMapping(value = "/populateDataFromServer/{name}", method=RequestMethod.PUT, produces={"application/json"})
-    public @ResponseBody List<Integer> populateActivePSwapBasketPUT(@PathVariable String name) {                
+    // @RequestMapping(value = "/populateDataFromServer/{name}", method=RequestMethod.PUT, produces={"application/json"})
+    // public @ResponseBody List<Integer> populateActivePSwapBasketPUT(@PathVariable String name) {                
         
-        if (name.equalsIgnoreCase("JavaHonk")) {
-            return returnDataList();
-        }else {
-            List<Integer> list = new ArrayList<Integer>();
-            list.add(12345);
-            return list;
-        }
-    }
+    //     if (name.equalsIgnoreCase("JavaHonk")) {
+    //         return returnDataList();
+    //     }else {
+    //         List<Integer> list = new ArrayList<Integer>();
+    //         list.add(12345);
+    //         return list;
+    //     }
+    // }
     
-    @RequestMapping(value = "/populateDataFromServer/{name}", method=RequestMethod.DELETE, produces={"application/json"})
-    public @ResponseBody List<Integer> populateActivePSwapBasketDelete(@PathVariable String name) {             
+    // @RequestMapping(value = "/populateDataFromServer/{name}", method=RequestMethod.DELETE, produces={"application/json"})
+    // public @ResponseBody List<Integer> populateActivePSwapBasketDelete(@PathVariable String name) {             
         
-        if (name.equalsIgnoreCase("JavaHonk")) {
-            return returnDataList();
-        }else {
-            List<Integer> list = new ArrayList<Integer>();
-            list.add(12345);
-            return list;
-        }
-    }
+    //     if (name.equalsIgnoreCase("JavaHonk")) {
+    //         return returnDataList();
+    //     }else {
+    //         List<Integer> list = new ArrayList<Integer>();
+    //         list.add(12345);
+    //         return list;
+    //     }
+    // }
     
     private List<Integer> returnDataList() {
         Random rand = new Random();
