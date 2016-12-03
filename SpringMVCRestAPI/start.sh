e41a5c31ad008612f0a321f031ecaa4fabb5cc98
no7dw <no7david@gmail.com>
2016-12-03 23:58:02 +0800

    add how to start in another way

--- SpringMVCRestAPI/start.sh
+++ SpringMVCRestAPI/start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 java -jar target/dependency/jetty-runner.jar target/*.war 
+# or
+# mvn package
+# mvn -Djetty.port=8090 jetty:run
+# now you can 
+# curl http://localhost:8090/popdata/1
