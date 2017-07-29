8b3066209396be3b68cae6b1b2d0e2e01083bdbb
no7dw <no7david@gmail.com>
2017-07-29 15:53:05 +0800

    add mvn projects

---
+++ mvn-test/hello-quickstart/src/main/java/com/wade/App.java
@@ -0,0 +1,14 @@
+package com.wade;
+
+/**
+ * Hello world!
+ *
+ */
+import org.slf4j.*;
+
+public class App {
+    public static void main(String[] args){
+        Logger logger = LoggerFactory.getLogger(App.class);
+        logger.info("Hello world");
+    }
+}
