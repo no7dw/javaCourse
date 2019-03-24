10451d53eb9dda047d803bc82d8087361009bf06
no7dw <no7david@gmail.com>
2019-03-24 19:42:51 +0800

    use Slf4j extern

--- LomTest/src/main/java/DataExample.java
+++ LomTest/src/main/java/DataExample.java
@@ -2,17 +2,20 @@
  * Created by dengwei on 2019/3/24.
  */
 import lombok.*;
-
+import lombok.extern.slf4j.Slf4j;
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
+@Slf4j
 public class DataExample implements Serializable {
     private final String name;
     private int age;
 
+
     public static void main(String[] args) {
         DataExample do1 = new DataExample("dengwei", 3);
+        log.info(do1.toString());
         System.out.println(do1.toString());
     }
 
