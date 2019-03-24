38598750b80e1f2bba23720bd6c7c22218a6b058
no7dw <no7david@gmail.com>
2019-03-24 21:53:22 +0800

    add more usage of lombok and add readme for pic

---
+++ LomTest/README.md
@@ -0,0 +1,8 @@
+### projectlombok example
+
+
+没安装plugin 的使用效果,没智能提示,因为不知道类型,而且IDE认为你有错, 见红色部分
+![ide1](https://wade-blog.oss-cn-shenzhen.aliyuncs.com/Pasted%20Graphic%201.png)
+按照lombok plugin 后,使用效果, 正常显示:
+![ide2](https://wade-blog.oss-cn-shenzhen.aliyuncs.com/Pasted%20Graphic%202.png)
+![ide3](https://wade-blog.oss-cn-shenzhen.aliyuncs.com/Pasted%20Graphic%203.png)
--- LomTest/src/main/java/DataExample.java
+++ LomTest/src/main/java/DataExample.java
@@ -4,19 +4,36 @@
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import java.io.Serializable;
+import java.util.HashMap;
 
 @Data
 @AllArgsConstructor
 @Slf4j
+@EqualsAndHashCode
 public class DataExample implements Serializable {
     private final String name;
     private int age;
 
-
     public static void main(String[] args) {
         DataExample do1 = new DataExample("dengwei", 3);
         log.info(do1.toString());
-        System.out.println(do1.toString());
-    }
+        System.out.println(do1.toString()); //toString()
+        System.out.println(do1.getName());//getter
 
+        val do2 = new DataExample("dengwei", 4);
+        do2.age = 5; //setter
+        System.out.println(do2.age);
+
+        System.out.printf("do1= do2 ? %s\n", do1.equals(do2)); //equal
+
+        val map = new HashMap<Integer , String>(); //val
+        map.put(0, "Jan");
+        map.put(1, "Feb");
+
+        for(val entry: map.entrySet()) {
+            System.out.printf("%d: %s\n",entry.getKey(),entry.getValue());
+        }
+        //Synchronized has not simplify much, and granularity(粒度) is not small enough
+        //so not recommended
+    }
 }
